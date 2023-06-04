a949aef8126d0da49ac61d7f63be8392664f4d71
Yuze Wang <yuzewang621@gmail.com>
2023-06-04 00:27:26 -0600

    rename

---
+++ db/schema/03_resources.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS resources CASCADE;
+CREATE TABLE resources (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  resource_url VARCHAR(255) NOT NULL,
+  photo_url VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
