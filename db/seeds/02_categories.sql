b339f4931638ae72adacb81aaf355ba798c8976e
Yuze Wang <yuzewang621@gmail.com>
2023-06-04 03:48:33 -0600

    add fake data for categories

---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,4 @@
+--Categories table seeds here
+INSERT INTO categories (name, description) VALUES('English','Description for english');
+INSERT INTO categories (name, description) VALUES('Math','Description for Math');
+INSERT INTO categories (name, description) VALUES('Internet','Description for Internet');
