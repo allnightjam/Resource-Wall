a4cb4162ca317025a1c7e75f213d6af138ad1cb4
Yuze Wang <yuzewang621@gmail.com>
2023-06-04 15:41:46 -0600

    remove comments

--- routes/resource.js
+++ routes/resource.js
@@ -3,21 +3,21 @@ const router  = express.Router();
 
 const resourceQueries = require('../db/queries/resources');
 
-// // GET route handler for /addResource
-// router.post('/', (req, res) => {
-//   const resource = req.body;
-//   resourceQueries.addResource(resource)
-//     .then((resource) => {
-//       if (!resource) {
-//         return res.send({ error: "error" });
-//       }
-//       res.redirect('/'); // Redirect to the index page
-//     })
-//     .catch((error) => {
-//       console.error("Error add resource:", error);
-//       res.status(500).json({ error: "Failed to add resource" });
-//     });
-// });
+// GET route handler for /addResource
+router.post('/', (req, res) => {
+  const resource = req.body;
+  resourceQueries.addResource(resource)
+    .then((resource) => {
+      if (!resource) {
+        return res.send({ error: "error" });
+      }
+      res.redirect('/'); // Redirect to the index page
+    })
+    .catch((error) => {
+      console.error("Error add resource:", error);
+      res.status(500).json({ error: "Failed to add resource" });
+    });
+});
 
 //GET route handler for /getAllResource
 // router.get('/', (req, res) => {
