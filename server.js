a2634d326734a04af1bbbfa83238836f979d3711
Joyce Ann Miguel <joyce.a.miguel@gmail.com>
2023-06-04 13:55:18 -0400

    server

--- server.js
+++ server.js
@@ -48,6 +48,100 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+/////////////////////////////////////
+const users = {
+  aJ48lW: {
+    id: "aJ48lW",
+    email: "user@example.com",
+    password: "$2a$10$xO9o0DnDc4hWZIXYPhU6V.5oufJcp7EwlA0MjPl/2wHcJfORiTXMK",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "$2a$10$xO9o0DnDc4hWZIXYPhU6V.5oufJcp7EwlA0MjPl/2wHcJfORiTXMK",
+  },
+};
+
+function generateRandomString() {
+  let result = "";
+  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const length = 6;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * length));
+    counter += 1;
+  }
+  return result;
+}
+
+const getUserByEmail = function(email, database) {
+  for (let user in database) {
+    if (database[user].email === email){
+      return database[user];
+    }
+  }
+  return null;
+};
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/login", (req,res) => {
+  const user = users[req.body.id]
+  const templateVars = { user: false };
+  res.render("login", templateVars);
+});
+
+app.get("/search", (req,res) => {
+  res.render("search");
+});
+
+app.get("/profile", (req,res) => {
+  res.render("profile");
+});
+
+app.post("/register", (req,res) => {
+  const user = getUserByEmail(req.body.email,users);
+  const newUser = {
+    id: generateRandomString(6),
+    email: req.body.email,
+    password: req.body.password,
+  };
+  if (req.body.email === "" || req.body.password === "") {
+    return res.status(400).send("Email And/Or Password Invalid");
+  }
+  if (user && user.email === newUser.email) {
+    return res.status(400).send("Email Taken");
+  }
+  users[newUser.id] = newUser;
+  req.body.id = newUser.id
+  res.redirect("/");
+});
+
+app.post("/login", (req,res) => {
+  const user = getUserByEmail(req.body.email, users);
+  if (!user) {
+    return res.status(403).send("Email And/Or Password Invalid");
+  }
+  if (req.body.password === user.password) {
+    req.body.id = user.id;
+    return res.redirect('/');
+  }
+  return res.status(403).send("Email And/Or Password Invalid");
+});
+
+app.post("/logout", (req,res) => {
+  req.session = null;
+  res.redirect("/login");
+});
+
+app.post("/profile", (req,res) => {
+  res.render("profile");
+});
+
+
