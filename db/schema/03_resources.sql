ff1c11e16bfda584e2dd2e0e190aa6822c028775
Yuze Wang <yuzewang621@gmail.com>
2023-06-05 01:45:27 -0600

    update url datatype from varchar to text

--- db/schema/03_resources.sql
+++ db/schema/03_resources.sql
@@ -3,8 +3,8 @@ CREATE TABLE resources (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
-  resource_url VARCHAR(255) NOT NULL,
-  photo_url VARCHAR(255) NOT NULL,
+  resource_url TEXT NOT NULL,
+  photo_url TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
