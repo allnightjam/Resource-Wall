580a391499d02192c69de3eeaef733d96fe0059a
Yuze Wang <yuzewang621@gmail.com>
2023-06-05 13:55:40 -0600

    add resource_id as parameter

--- public/scripts/resource.js
+++ public/scripts/resource.js
@@ -1,8 +1,8 @@
 
 $('.start-radio').on("change", function(event) {
   event.preventDefault();
-  const data = $('input[name=rating]:checked').val();
-  console.log(data);
+  // const data = $('input[name=rating]:checked').val();
+  const data = {rating:$('input[name=rating]:checked').val(), 'resource-id':$('#rating-resource-id').val()};
 
   $.post("/rating", data)
     .then(() => {
