05a8c852b14796414c54e3594cfa3169cb7bcb77
Yuze Wang <yuzewang621@gmail.com>
2023-06-05 08:50:54 -0600

    query for comments

---
+++ db/queries/resourceComments.js
@@ -0,0 +1,50 @@
+const db = require('../connection');
+
+const addComments = function(comment,resourceId,userId) {
+  
+  const queryString = `INSERT INTO resource_comments(comment, resource_id, user_id) VALUES($1, $2, $3) RETURNING *`;
+  
+  return db.query(queryString, [comment,resourceId, userId])
+    .then(data => {
+      console.log("add comment data into db -----------", data.rows);
+      return data.rows;
+    })
+    .catch(error => {
+      console.error("Error insert resource_comments in queries:", error);
+      throw error;
+    });
+};
+
+const getComments = function(resourceId) {
+  
+  const queryString = `SELECT resource_comments.*, users.username, users.avatar 
+        FROM resource_comments 
+        LEFT JOIN users ON user_id = users.id
+        WHERE resource_id = $1`;
+  
+  return db.query(queryString, [resourceId])
+    .then(data => {
+      console.log("get comment from db -----------", data.rows);
+      return data.rows;
+    })
+    .catch(error => {
+      console.error("Error get comment from resource_comments in queries:", error);
+      throw error;
+    });
+};
+
+const getTotalComments = function(resourceId) {
+  
+  const queryString = `SELECT count(*) as total_comments FROM resource_comments WHERE resource_id = $1`;
+  
+  return db.query(queryString, [resourceId])
+    .then(data => {
+      return data.rows;
+    })
+    .catch(error => {
+      console.error("Error get total comment from resource_comments in queries:", error);
+      throw error;
+    });
+};
+
+module.exports = {addComments, getComments,getTotalComments};
