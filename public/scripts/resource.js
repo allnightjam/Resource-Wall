11f1e7d45d731aa531c013f8a6b4b30032bbe083
Yuze Wang <yuzewang621@gmail.com>
2023-06-05 19:27:49 -0600

    add comments

--- public/scripts/resource.js
+++ public/scripts/resource.js
@@ -12,3 +12,34 @@ $('.start-radio').on("change", function(event) {
       console.log("error occured");
     });
 });
+
+// $(() => {
+//   let isLiked = false;
+//   $('.fa-heart').on("click", function (event) {
+//     event.preventDefault();
+//     isLiked = !isLiked; // Toggle the liked state
+//     if (isLiked) {
+//       this.classList.add('liked');
+//     } else {
+//       this.classList.remove('liked');
+//     }
+  
+//     // Perform further actions based on the liked/unliked state
+//     if (isLiked) {
+//       // Code for when the icon is liked
+//       console.log('Icon is liked');
+//     } else {
+//       // Code for when the icon is unliked
+//       console.log('Icon is unliked');
+//     }
+//     $.post("/addlikes", isLiked)
+//       .then(() => {
+
+//         console.log("liked successful!");
+//       })
+//       .catch(function () {
+//         console.log("error occured");
+//       });
+//   });
+// });
+
