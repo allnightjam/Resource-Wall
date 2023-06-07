0905dc625bfa6578ffc44bcfd130a661d6ad21c7
Wei Huang <huangwei19810803@gmail.com>
2023-06-07 16:36:57 -0600

    fix like part in main page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,36 @@
 //Client facing scripts here
-$(document).ready(function(){
-  $('.category-link').click(function(){
-    console.log('---clicked category--');
-    $('.category-link a').removeClass("category-active");
-    $(this).addClass("category-active");
-});
-});
+$('.fa-heart').click(function() {
+  //var val = parseInt($(this).text(), 10);
 
+  const $curHeartIcon = $(this);
+
+  const $curForm = $curHeartIcon.closest('form');
+  const $curResourceInput = $curForm.find('input');
+  const $heartNum = $curForm.find('span');
+
+  let val = $heartNum.html();
+  let resourceID = $curResourceInput.val();
+  console.log(Number(val));
+  console.log(Number(resourceID));
+
+
+  $(this).toggleClass('is-liked');
+
+  if ($(this).hasClass('is-liked')) {
+    $(this).addClass('fa-solid');
+    $(this).removeClass('fa-regular');
+    val++
+    //likesSubmitForm(Number(resourceID));
+  } else {
+    $(this).addClass('fa-regular');
+    $(this).removeClass('fa-solid');
+    val--
+    // User removed his like
+    //unLikesSubmitForm(Number(resourceID));
+  }
+
+  $($heartNum).text(val);
+});
 
 // Get the like icon element
 
@@ -14,3 +38,4 @@ const  likesSubmitForm = function(resourceId) {
   // Submit the form corresponding to the clicked icon
   document.getElementById('add-likes-form-' + resourceId).submit();
 };
+
