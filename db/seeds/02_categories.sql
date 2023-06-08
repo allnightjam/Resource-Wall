f09c8f621d1803d257f20bf26eb49eed2b15be77
Joyce Ann Miguel <joyce.a.miguel@gmail.com>
2023-06-08 16:53:57 -0400

    adding data to test out features

--- db/seeds/02_categories.sql
+++ db/seeds/02_categories.sql
@@ -1,6 +1,6 @@
 --Categories table seeds here
 INSERT INTO categories (name, description) VALUES('Educational','Description for Educational');
-INSERT INTO categories (name, description) VALUES('Lifestyle','Description for MatLifestyle');
+INSERT INTO categories (name, description) VALUES('Lifestyle','Description for Lifestyle');
 INSERT INTO categories (name, description) VALUES('Sports & Activities','Description for Sports & Activities');
 INSERT INTO categories (name, description) VALUES('Entertainment','Description for Entertainment');
 INSERT INTO categories (name, description) VALUES('Miscellaneous','Description for Miscellaneous');
